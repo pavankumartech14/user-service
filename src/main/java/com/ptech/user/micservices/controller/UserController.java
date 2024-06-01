ce2dbbdcb0fc911b197075a4212e26b58ee778da
Pavan Kumar G <gurrampavan.k@clinevotech.com>
2024-06-01 14:42:20 +0530

    upload Proj to git hub

---
+++ src/main/java/com/ptech/user/micservices/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.ptech.user.micservices.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@GetMapping("/getString")
+	public String getName() { 
+		return "This is Microservices Test Priject";
+	}
+}
